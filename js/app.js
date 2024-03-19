e69c4de262e170d1f279cc79fdc86672c6eb81c0
tadeo <tadeoacosta14@gmail.com>
2024-03-19 00:28:43 -0300

    primeros ejercicios de javascript en rollincode

---
+++ js/app.js
@@ -0,0 +1,27 @@
+//1 ejercicio
+/*
+alert("un mensaje");
+//2 ejercicio
+document.write("Hello  World!");
+
+
+//3 ejercicio
+document.write(3 + 5);
+
+/4.- Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
+Ejemplo: 
+
+input: Coder 
+Output: Hola Coder
+
+const coder = prompt("Dime tu nombre de usuario:");
+
+document.write(`hola ${coder}`);
+
+ejercicio 5
+Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+Nota: Tener en cuenta la siguiente función: parseInt
+*/
+const num1 = parseInt(prompt('ingrese un numero 1'));
+const num2 = parseInt(prompt('ingrese un numero 2'));
+const num3 = parseInt(prompt('ingrese un numero 3'));
