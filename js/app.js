3c3ed16b8e3d6af9a52fbd5221d728197b38ef79
tadeo05 <tadeoacosta14mail.com>
2024-03-25 09:12:48 -0300

    se agrego la opcion en la que si serian 3 numeros, tambien se puso el caso en el que si serian 2 iguale y uno disitinto menor

--- js/app.js
+++ js/app.js
@@ -57,20 +57,30 @@ Escribe un programa que pida 3 numeros y escriba en la pantalla el mayor de los 
 */
 const num1 = parseInt(prompt('ingrese un numero 1'));
 const num2 = parseInt(prompt('ingrese un numero 2'));
+const num3 = parseInt(prompt('ingrese un numero 3'));
 
 
-function mayor (){
-    if (num1 > num2) {
+function mayor (num1,num2,num3){
+    if (num1 > num2 && num1 > num3) {
         console.log (`El ${num1} es el mas grande`);
-    } else if (num2>num1){
+    } else if (num2 > num1 && num2 > num3){
         console.log (`El ${num2} es el mas grande`)
-    } else {
-        console.log (`son iguales`)
+    } else if (num3 > num1 && num3 > num2){
+        console.log (`El ${num3} es el mas grande`)
+    } else if (num1 === num2 && num1 !== num3) {
+        console.log(`El ${num1} y el ${num2} son los mayores`);
+    } else if (num1 === num3 && num1 !== num2) {
+        console.log(`El ${num1} y el ${num3} son los mayores`);
+    } else if (num2 === num3 && num2 !== num1) {
+        console.log(`El ${num2} y el ${num3} son los mayores`);
+     } else {
+        console.log (`Son todos iguales`)
     }
+    
 }
 
 
-mayor ()
+mayor (num1, num2, num3)
 
 
 
