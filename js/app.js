9255b339d353c10e0eb87bfd8745b161ce0d4dbf
tadeo05 <tadeoacosta14mail.com>
2024-03-25 08:47:18 -0300

    agregamos el ejercicio 5.1

--- js/app.js
+++ js/app.js
@@ -19,11 +19,37 @@ const coder = prompt("Dime tu nombre de usuario:");
 document.write(`hola ${coder}`);
 
 ejercicio 5
-Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+Escribe un programa de tres líneas que pida un número, pida otro número y 
+escriba el resultado de sumar estos dos números.
 Nota: Tener en cuenta la siguiente función: parseInt
 
 const num1 = parseInt(prompt('ingrese un numero 1'));
 const num2 = parseInt(prompt('ingrese un numero 2'));
 const resultado = num1 + num2;
 document.write(`El resultado es ${resultado}`);
+
+ejercicio 5.1
+
+escriba un programa que pida dos numeros y escriba en la pantalla cual es el mayor
 */
+
+const num1 = parseInt(prompt('ingrese un numero 1'));
+const num2 = parseInt(prompt('ingrese un numero 2'));
+
+function mayor (){
+    if (num1 > num2) {
+        console.log (`El ${num1} es el mas grande`);
+    } else if (num2>num1){
+        console.log (`El ${num2} es el mas grande`)
+    } else {
+        console.log (`son iguales`)
+    }
+}
+document.log(`El resultado es ${resultado}`);
+
+
+
+
+
+
+
