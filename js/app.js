f173e1f017707f6bfe019b86331e96142967de5b
tadeo <tadeoacosta14@gmail.com>
2024-04-04 00:21:07 -0300

    ejercicio 10

--- js/app.js
+++ js/app.js
@@ -112,7 +112,7 @@ Nota: Tener en cuenta la función length y substring o charAt (developer mozilla
 
 Ejemplo:
 input: Hola mundo           Output: oauo
-*/
+
 
 let phrase = prompt('Escribe una frase');
 phrase = phrase.toLowerCase(); //para pasar a minuscula
@@ -128,5 +128,30 @@ for (let i = 0; i < phrase.length; i++) {
 		document.write(phrase.substr(i, 1));
 	}     //con el substr extraemos las vocales, luego con el bucle for  vamos comparando cada letra de la fr
 }
+
+10.- Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o
+7 (sólo hay que comprobar si lo es por uno de los cuatro)
+Ejemplo:
+input: 20 Output: El 20 es divisible por 2.
+
+*/
+
+function divisible() {
+    const n = prompt("Ingrese un número: ");
+    
+    if (n % 2 === 0) {
+        console.log(`El ${n} es divisible por 2.`);
+    } else if (n % 3 === 0) {
+        console.log(`El ${n} es divisible por 3.`);
+    } else if (n % 5 === 0) {
+        console.log(`El ${n} es divisible por 5.`);
+    } else if (n % 7 === 0) {
+        console.log(`El ${n} es divisible por 7.`);
+    } else {
+        console.log(`El ${n} no es divisible por ninguno de los números anteriores.`);
+    }
+}
+
+divisible();
 
 
